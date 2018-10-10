89ce6222161612fc72573d81475fdfdb9c0bbd36
Rafal Leszczynski <info@grafikart.pl>
2018-10-10 22:49:39 +0200

    final1.0

---
+++ modules/OSinfo.js
@@ -0,0 +1,28 @@
+'use scrict';
+
+var timeCount = require('../modules/timeF')
+var os = require('os');
+
+function getOSinfo() {
+  var type = os.type();
+  if (type === 'Darwin') {
+    type = 'OSX';
+  } else if (type === 'Windows_NT') {
+    type = 'Windows';
+  }
+
+  var release = os.release();
+  var cpu = os.cpus()[0].model;
+  var uptime = os.uptime();
+  var userInfo = os.userInfo();
+  console.log('System:', type);
+  console.log('Release:', release);
+  console.log('CPU model:', cpu);
+  console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+  console.log('Uptime:', timeCount.print(uptime) );
+  console.log('User name:', userInfo.username);
+  console.log('Home dir:', userInfo.homedir);
+}
+
+exports.print = getOSinfo;
+
